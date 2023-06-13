e0cc16d2316492a10e1205e3d4384bc332ac9ee0
Pedramrezaei04 <pedramrezaei04@gmail.com>
2023-06-13 16:54:47 +0200

    Add files via upload

--- BWM_web/settings.py
+++ BWM_web/settings.py
@@ -122,8 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'BWM_solver/static'),
+    os.path.join(BASE_DIR, 'BWM_solver/static', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
